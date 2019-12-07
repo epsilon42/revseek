5fa68f35e2913857dc0f10d8925171a5cfea58d0
epsilon42 <andrew@goninon.net>
2019-12-07 11:49:14 +0800

    Updates

---
+++ precache-manifest.24489b38a9550d396d9d4ee565c74ee9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fd28f5987131a2f344d73ef25f71e6",
+    "url": "/revseek/index.html"
+  },
+  {
+    "revision": "7c48d821575f74d0f99c",
+    "url": "/revseek/static/css/main.02f0d6ea.chunk.css"
+  },
+  {
+    "revision": "67918e618755b41f7bd9",
+    "url": "/revseek/static/js/2.5bd34373.chunk.js"
+  },
+  {
+    "revision": "7c48d821575f74d0f99c",
+    "url": "/revseek/static/js/main.ff7ebd2b.chunk.js"
+  },
+  {
+    "revision": "a00af0ae9796c5a0e246",
+    "url": "/revseek/static/js/runtime-main.ac25582e.js"
+  }
+]);
