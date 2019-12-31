4dfbc58f998903f1df800be4d6b52405d0a4a52c
epsilon42 <andrew@goninon.net>
2019-12-31 13:24:05 +0800

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-37683605-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById("root"));
